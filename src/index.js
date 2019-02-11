48c7efe4af8655e610cc94c1f099a3ad45ef1b19
Student <Student@LVAGPLTP0909_BC.dir.svc.accenture.com>
2019-02-11 14:38:32 +0200

    REF: Initial commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// const element = <h1> JSX Code</h1>;
+// ReactDOM.render(<App />, document.getElementById(element));
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
